2ece58040785a07ce6657e59448016898dd14ab7
Zet992 <rodionovalexey11@mail.ru>
2021-06-26 22:51:41 +0300

    work with pycharm

--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost/flaskdata"
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
